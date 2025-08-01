ba123f87a59fc4a6d19dae4085d1472caa54e297
Elaihorng <151163076+Elaihorng@users.noreply.github.com>
2025-08-01 12:09:11 +0700

    Describe your update web

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-use App\Http\Controllers\Frontend\HomeController;
+use App\Http\Controllers\frontend\HomeController;
 use App\Http\Controllers\frontend\PageController;
 use App\Http\Controllers\frontend\ProductController;
 use App\Http\Controllers\frontend\CartController;
